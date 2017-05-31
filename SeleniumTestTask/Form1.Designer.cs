617cf56c7958b02d7329d1f6efa27881a5807b48
petru4o4o <petrol89@gmail.com>
2017-05-31 14:35:50 +0300

    added

---
+++ SeleniumTestTask/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SeleniumTestTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this._radioChrome = new System.Windows.Forms.RadioButton();
+            this._radioMozilla = new System.Windows.Forms.RadioButton();
+            this._logBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go try";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // _radioChrome
+            // 
+            this._radioChrome.AutoSize = true;
+            this._radioChrome.Location = new System.Drawing.Point(12, 26);
+            this._radioChrome.Name = "_radioChrome";
+            this._radioChrome.Size = new System.Drawing.Size(61, 17);
+            this._radioChrome.TabIndex = 1;
+            this._radioChrome.TabStop = true;
+            this._radioChrome.Text = "Chrome";
+            this._radioChrome.UseVisualStyleBackColor = true;
+            this._radioChrome.CheckedChanged += new System.EventHandler(this._radioChrome_CheckedChanged);
+            this._radioChrome.Click += new System.EventHandler(this._radioChrome_Click);
+            // 
+            // _radioMozilla
+            // 
+            this._radioMozilla.AutoSize = true;
+            this._radioMozilla.Location = new System.Drawing.Point(12, 70);
+            this._radioMozilla.Name = "_radioMozilla";
+            this._radioMozilla.Size = new System.Drawing.Size(57, 17);
+            this._radioMozilla.TabIndex = 2;
+            this._radioMozilla.TabStop = true;
+            this._radioMozilla.Text = "Mozilla";
+            this._radioMozilla.UseVisualStyleBackColor = true;
+            this._radioMozilla.CheckedChanged += new System.EventHandler(this._radioMozilla_CheckedChanged);
+            this._radioMozilla.Click += new System.EventHandler(this._radioMozilla_Click);
+            // 
+            // _logBox
+            // 
+            this._logBox.Location = new System.Drawing.Point(12, 156);
+            this._logBox.Multiline = true;
+            this._logBox.Name = "_logBox";
+            this._logBox.Size = new System.Drawing.Size(578, 126);
+            this._logBox.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(515, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Bye!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(443, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Close Browser";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 334);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this._logBox);
+            this.Controls.Add(this._radioMozilla);
+            this.Controls.Add(this._radioChrome);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Selenium Test Task";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton _radioChrome;
+        private System.Windows.Forms.RadioButton _radioMozilla;
+        private System.Windows.Forms.TextBox _logBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SeleniumTestTask/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+
+
+namespace SeleniumTestTask
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver Browser;
+        bool _isEnglishLang = false;
+        bool _isRussianLang = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Select browser by default
+            _radioChrome.Checked = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region Initialize browser regarding to choise
+            if (_radioChrome.Checked)
+            {
+                Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            }
+            if (_radioMozilla.Checked)
+            {
+                Browser = new OpenQA.Selenium.Firefox.FirefoxDriver();
+            }
+            Browser.Manage().Window.Maximize();
+            _logBox.AppendText("Browser object initialized. Type: " + Browser.GetType().ToString());
+            #endregion
+
+            bool _isTestSuiteFinishedOK = false;
+            #region Try to run set of actions
+            try
+            {
+                #region Login
+                _logBox.AppendText("Try go to facebook.\n");
+                Browser.Navigate().GoToUrl("https://www.facebook.com/");
+
+                //Login
+                IWebElement insertmail = Browser.FindElement(By.Id("email"));
+                if (insertmail != null)
+                    _logBox.AppendText("Found where to input email\n");
+                insertmail.SendKeys("olegseleniumtest@i.ua");
+
+                IWebElement isertpass = Browser.FindElement(By.Id("pass"));
+                if (isertpass != null)
+                    _logBox.AppendText("Found where to input pass\n");
+                isertpass.SendKeys("seleniumtesttask");
+
+                IWebElement LoginButton = Browser.FindElement(By.Id("loginbutton"));
+                LoginButton.Click();
+                _logBox.AppendText("Loging in.\n");
+                #endregion
+                #region  Check activated language from 2 options: English and Russian
+                IWebElement checkElement;
+                try
+                {
+                    checkElement = Browser.FindElement(By.PartialLinkText("Home")); //if it will initialize language is English
+                    _isEnglishLang = true;
+                }
+                catch
+                {
+                    _isRussianLang = true;
+                }
+                finally
+                {
+                    if (!_isEnglishLang && !_isRussianLang)
+                    {
+                        _logBox.AppendText("Something wrong with languages.\n");
+                    }
+                }
+
+                //if (checkElement == null) //in case if not, i will take Russian as default, such a twist
+                //{
+                   
+                //}
+                
+
+                if (_isEnglishLang)
+                {
+                    _logBox.AppendText("Current language - English.\n");
+                }
+                else
+                {
+                    _logBox.AppendText("Current language - Russian.\n");
+                }
+                
+               
+                #endregion
+                #region Change lang
+                //this time i will not seek for element, i can go directly to url, why not? 
+                _logBox.AppendText("Try to go to language settings.\n");
+                Browser.Navigate().GoToUrl("https://www.facebook.com/settings?tab=language&section=account");
+
+
+                string languageSelectByText = string.Empty;
+                string languageSelectByValue = string.Empty;
+                if (_isEnglishLang)
+                {
+                    languageSelectByText = "Русский";
+                    languageSelectByValue = "ru_RU";
+                }
+                else //I will use this a bit later just to demonstrate2 ways
+                {
+                    if (_isRussianLang)
+                    {
+                        languageSelectByText = "English";
+                        languageSelectByValue = "en_US";
+                    }
+                    else
+                    {
+                        _logBox.AppendText("Whaaat? Something wrong with languages! ");
+                        throw new Exception("TS failed by Olegs laziness.");
+                    }
+                }
+                    
+                
+
+                
+                IWebElement changelangDropList = Browser.FindElement(By.Name("new_language"));
+                SelectElement selectLanguage = new SelectElement(changelangDropList);
+
+                //Just to show different ways
+                if (_isEnglishLang)
+                    selectLanguage.SelectByText(languageSelectByText);
+                else
+                    selectLanguage.SelectByValue(languageSelectByValue);
+                _logBox.AppendText("Language option changed to: " + languageSelectByText + "\n");
+
+                //Save Changes
+                //Here i got problems with clicking. As i found on forums - delay should help. It didnt.
+                //So i defined id of button for each language 
+                //string buttonSubmitId = string.Empty;
+                //if (_isEnglishLang)
+                //{
+                //    buttonSubmitId ="u_0_7";
+                //}
+                //if (_isRussianLang)
+                //{
+                //    buttonSubmitId = "u_4_2";
+                //}
+                string submitButtonXPATH = "//input[@type='submit']";
+                IWebElement submit = Browser.FindElement(By.XPath(submitButtonXPATH));
+                //problem is here-->             
+                submit.Click();
+                _logBox.AppendText("Changes Submitted.\n");
+
+                #endregion
+                #region Final
+                _isTestSuiteFinishedOK = true;
+                #endregion
+            }
+            catch (Exception runex)
+            {
+                _logBox.AppendText("\n\n" + runex.Message);
+            }
+            #endregion
+            MessageBox.Show("Test suite result: " + _isTestSuiteFinishedOK);
+        }
+
+        private void _radioChrome_CheckedChanged(object sender, EventArgs e)
+        {
+            bool isCheckedC = _radioChrome.Checked;
+        }
+
+        private void _radioChrome_Click(object sender, EventArgs e)
+        {
+            _radioMozilla.Checked = false;
+            _radioChrome.Checked = true;
+            _logBox.AppendText("Chrome selected.\n");
+        }
+        private void _radioMozilla_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void _radioMozilla_Click(object sender, EventArgs e)
+        {
+            _radioMozilla.Checked = true;
+            _radioChrome.Checked = false;
+            _logBox.AppendText("Mozarella selected.\n");           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Browser.Quit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Browser.Quit();
+            Application.Exit();
+        }
+    }
+}
